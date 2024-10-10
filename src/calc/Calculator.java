b9679a8c5514613f6c39a23ee6060441efc645eb
Amna555673 <F223633@cfd.nu.edu.pk>
2024-10-10 09:45:44 +0500

    Added addition and multiplication methods.

---
+++ src/calc/Calculator.java
@@ -0,0 +1,18 @@
+package calc;
+
+public class Calculator {
+
+    // Method for addition
+    public int addition(int a, int b) {
+       
+        return a + b;
+    }
+
+    // Method for multiplication
+    public int multiplication(int a, int b) {
+    
+        return a * b;
+    }
+
+   
+}
